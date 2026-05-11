801e7b4cc622c13f2d8038af5a22e7767347f511
Francy <187552164+Francy-Coder@users.noreply.github.com>
2026-05-11 13:22:43 +0200

    Create UtenteDao.java

---
+++ Codes/2.0/ParteSistemi/UtenteDao.java
@@ -0,0 +1,27 @@
+package com.example.visiongo.database.dao;
+
+import androidx.room.*;
+import com.example.visiongo.database.entities.Utente;
+import java.util.List;
+
+@Dao
+public interface UtenteDao {
+
+    @Insert
+    long inserisci(Utente utente);
+
+    @Update
+    void aggiorna(Utente utente);
+
+    @Delete
+    void elimina(Utente utente);
+
+    @Query("SELECT * FROM utenti WHERE email = :email LIMIT 1")
+    Utente trovaPeremail(String email);
+
+    @Query("SELECT * FROM utenti WHERE id_utente = :id LIMIT 1")
+    Utente trovaPerID(int id);
+
+    @Query("SELECT * FROM utenti")
+    List<Utente> tuttiGliUtenti();
+}
